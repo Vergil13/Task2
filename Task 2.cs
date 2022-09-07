4e5a19eb483dae1843761f9a096310aaecebeb17
Вергилий Двачевский <Vergils2ch@gmail.com>
2022-09-07 20:58:21 +0300

    я не понимаю почему не работает пуш

---
+++ Task 2.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите число");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число");
+int y=Convert.ToInt32(Console.ReadLine());
+if (x>y)
+    Console.WriteLine(x);
+    else
+        Console.WriteLine( y);
